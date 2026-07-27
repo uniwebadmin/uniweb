77982cd8219aaacd0e434c2d6f503ed45c89d046
Abdul Barik <startelecom620@gmail.com>
2026-07-27 20:10:23 +0530

    Polish portal experience

--- header.php
+++ header.php
@@ -55,6 +55,7 @@ if (($isMerchant || $isAdmin) && !headers_sent()) {
     <link rel="stylesheet" href="<?= APP_URL ?>/assets/css/uniweb.min.css?v=20260724b">
     <link rel="stylesheet" href="<?= APP_URL ?>/assets/css/theme-light.css?v=20260724b">
     <link rel="stylesheet" href="<?= APP_URL ?>/assets/css/public-pages.css?v=20260724b">
+    <link rel="stylesheet" href="<?= APP_URL ?>/assets/css/portal-polish.css?v=20260727a">
     <?php if (!empty($customerPortalUi)): ?>
     <link rel="stylesheet" href="<?= APP_URL ?>/assets/css/customer-portal.css?v=20260724b">
     <?php endif; ?>
